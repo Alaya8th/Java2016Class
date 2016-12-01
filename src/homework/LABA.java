199b08f94359247f637cf76167f173298f89b829
Administrator <Administrator@Asus>
2016-12-01 14:04:03 +0800

    12/01 My modification.

--- src/homework/LABA.java
+++ src/homework/LABA.java
@@ -10,6 +10,7 @@ import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class LABA {
 int a,b,c,mo,out,la,num;
@@ -49,9 +50,9 @@ String mon,laa;
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel a1 = new JLabel("New label");
-		a1.setIcon(new ImageIcon(LABA.class.getResource("/homework/p1.jpg")));
-		a1.setBounds(42, 49, 150, 150);
+		JLabel a1 = new JLabel("");
+		a1.setIcon(new ImageIcon(LABA.class.getResource("/homework/7.png")));
+		a1.setBounds(42, 10, 206, 230);
 		frame.getContentPane().add(a1);
 		
 		JLabel a2 = new JLabel("New label");
@@ -65,8 +66,8 @@ String mon,laa;
 		frame.getContentPane().add(a3);
 		
 		JLabel b1 = new JLabel("New label");
-		b1.setIcon(new ImageIcon(LABA.class.getResource("/homework/p1.jpg")));
-		b1.setBounds(258, 49, 150, 150);
+		b1.setIcon(new ImageIcon(LABA.class.getResource("/homework/7.png")));
+		b1.setBounds(258, 12, 205, 227);
 		frame.getContentPane().add(b1);
 		
 		JLabel b2 = new JLabel("New label");
@@ -80,8 +81,8 @@ String mon,laa;
 		frame.getContentPane().add(b3);
 		
 		JLabel c1 = new JLabel("New label");
-		c1.setIcon(new ImageIcon(LABA.class.getResource("/homework/p1.jpg")));
-		c1.setBounds(473, 49, 150, 150);
+		c1.setIcon(new ImageIcon(LABA.class.getResource("/homework/7.png")));
+		c1.setBounds(473, 10, 201, 230);
 		frame.getContentPane().add(c1);
 		
 		JLabel c2 = new JLabel("New label");
@@ -94,22 +95,22 @@ String mon,laa;
 		c3.setBounds(473, 49, 150, 150);
 		frame.getContentPane().add(c3);
 		
-		JLabel lblNewLabel = new JLabel("現有金額");
-		lblNewLabel.setBounds(42, 259, 76, 31);
-		frame.getContentPane().add(lblNewLabel);
+		JLabel lblMoney = new JLabel("您投注的總金額：");
+		lblMoney.setBounds(42, 298, 108, 24);
+		frame.getContentPane().add(lblMoney);
 		
-		JLabel money = new JLabel("100");
-		money.setFont(new Font("新細明體", Font.BOLD, 14));
-		money.setBounds(128, 267, 87, 21);
-		frame.getContentPane().add(money);
+		JLabel lblTotal = new JLabel("100");
+		lblTotal.setFont(new Font("新細明體", Font.BOLD, 14));
+		lblTotal.setBounds(144, 300, 87, 21);
+		frame.getContentPane().add(lblTotal);
 		
-		JLabel lblNewLabel_1 = new JLabel("投注金額");
-		lblNewLabel_1.setBounds(42, 348, 57, 15);
+		JLabel lblNewLabel_1 = new JLabel("投注金額：");
+		lblNewLabel_1.setBounds(42, 345, 77, 18);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		txt = new JTextField();
 		txt.setText("1");
-		txt.setBounds(128, 345, 137, 31);
+		txt.setBounds(144, 332, 137, 24);
 		frame.getContentPane().add(txt);
 		txt.setColumns(10);
 		
@@ -144,7 +145,7 @@ String mon,laa;
 				
 				mon=txt.getText();
 				mo=Integer.parseInt(mon);
-				laa=money.getText();
+				laa=lblTotal.getText();
 				la=Integer.parseInt(laa);
 				num=-1*mo;
 				
@@ -153,7 +154,7 @@ String mon,laa;
 				if (a==3 && b==3 && c==3) num+=(2*mo);
 			
 				out=la+num;
-				money.setText(""+out);
+				lblTotal.setText(""+out);
 				
 				if(out<=0){
 					btn.setEnabled(false);
@@ -166,5 +167,15 @@ String mon,laa;
 		
 		btn.setBounds(373, 316, 190, 47);
 		frame.getContentPane().add(btn);
+		
+		JLabel lblNewLabel_2 = new JLabel("目前累積獎金：");
+		lblNewLabel_2.setBounds(42, 267, 92, 21);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblPrice = new JLabel("1000000");
+		lblPrice.setForeground(Color.RED);
+		lblPrice.setFont(new Font("新細明體", Font.BOLD, 14));
+		lblPrice.setBounds(144, 267, 87, 21);
+		frame.getContentPane().add(lblPrice);
 	}
 }
